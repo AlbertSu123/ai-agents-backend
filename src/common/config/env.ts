f9f2530206493b4cfb0067edcf6476fe59a2ba9c
Albert Su <albertsu2001@gmail.com>
2025-02-01 11:41:05 -0800

    ✅remove unused env

--- src/common/config/env.ts
+++ src/common/config/env.ts
@@ -1,5 +1,4 @@
 import { ConfigModule } from '@nestjs/config';
-import * as fs from 'fs';
 import * as Joi from 'joi';
 
 const envVars = () => ({
@@ -7,21 +6,6 @@ const envVars = () => ({
   privy: {
     appId: process.env.PRIVY_APP_ID,
     appSecret: process.env.PRIVY_APP_SECRET,
-    verificationKey: fs.readFileSync(
-      'src/common/helpers/privy/verification.key.pem',
-    ),
-  },
-  moonpay: {
-    secretKey: process.env.MOONPAY_SECRET_KEY,
-  },
-  birdeye: {
-    apiKey: process.env.BIRDEYE_API_KEY,
-  },
-  helius: {
-    apiKey: process.env.HELIUS_API_KEY,
-  },
-  gasWallet: {
-    privateKey: process.env.GAS_WALLET_PRIVATE_KEY_BS58,
   },
   port: 80,
 });
@@ -30,10 +14,6 @@ const validationSchema = Joi.object({
   DATABASE_URL: Joi.string().required(),
   PRIVY_APP_ID: Joi.string().required(),
   PRIVY_APP_SECRET: Joi.string().required(),
-  MOONPAY_SECRET_KEY: Joi.string().required(),
-  BIRDEYE_API_KEY: Joi.string().required(),
-  HELIUS_API_KEY: Joi.string().required(),
-  GAS_WALLET_PRIVATE_KEY_BS58: Joi.string().required(),
 });
 
 export const EnvConfig = ConfigModule.forRoot({
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,5 @@
-import {
-  Body,
-  Controller,
-  HttpCode,
-  Inject,
-  Post,
-  Req,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, HttpCode, Inject, Post, Req } from '@nestjs/common';
 import { JwtRequired } from 'src/common/decorators/jwt.decorator';
-import { AuthGuard } from 'src/common/guards/auth.guard';
 import { Request } from '../common/dto/global.dto';
 import { UserService } from './user.service';
 import { GetOrCreateUserDto } from './user.dto';
@@ -18,14 +9,13 @@ export class UserController {
   constructor(@Inject(UserService) private userService: UserService) {}
 
   @JwtRequired(true)
-  @UseGuards(AuthGuard)
   @Post('/')
   @HttpCode(200)
   async createUser(@Req() request: Request, @Body() body: GetOrCreateUserDto) {
-    const user = await this.userService.get({ id: request.jwtData.sub });
+    const user = await this.userService.get({ id: '1' });
     if (!user) {
       const newUser = await this.userService.create({
-        id: request.jwtData.sub,
+        id: '1',
         address: body.address,
       });
       await this.userService.save(newUser);
